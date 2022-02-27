2b6a74c37cfff0812ae97670b2c787f05414e94a
Satyam kant <satyamkant2805@gmail.com>
2022-02-27 11:24:20 +0530

    added trees algorithms

--- TreeRepresentation.cpp
+++ TreeRepresentation.cpp
@@ -4,6 +4,37 @@ using namespace std;
 
 #define int long long
 
+class Node
+{
+public:
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val)
+    {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+
 int32_t main()
 {
+    Node *root = new Node(1);
+
+    root->left = new Node(2);
+    root->right = new Node(3);
+
+    root->left->right = new Node(4);
 }
+
+/*///////////////////////////////////////
+generated tree.....
+
+             1
+            / \
+           2   3
+            \
+             4
+//////////////////////////////////////*/
